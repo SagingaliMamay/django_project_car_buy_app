cb329316253b8d407d04d4c071c0c4b02448d593
Saga <sagingali.mamayev@gmail.com>
2021-11-23 13:43:39 +0100

    empty test

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('id')
+    return render(request, 'main/index.html', {'title': 'Main page', 'tasks' : tasks})
 
 
 def home(request):
