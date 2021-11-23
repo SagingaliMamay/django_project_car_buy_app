1cacda772d1b92c51e3c7ae9895baf8edd82cfd4
Saga <sagingali.mamayev@gmail.com>
2021-11-23 11:43:51 +0100

    migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('Merk : ', max_length=50)
+    task = models.TextField( 'Beschrijving:')
+
+    def __str__(self):
+        return self.title
+
